f78ca57e7ebb3e9dfdec08ca89835f28a5836a7a
Dylan-barnard <barnardd@stu.otc.school.com>
2025-05-12 15:05:02 +1200

    Added files

---
+++ Main code.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+from tkinter import *
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
+bg_color = "white"
+fg_color = "black"
